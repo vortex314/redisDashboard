eed1fd339e9259bea681c7cbc2448487745ab722
Lieven Merckx <lieven.merckx@ing.com>
2022-04-21 08:59:35 +0200

    Timers added

--- src/components/RedisWs.vue
+++ src/components/RedisWs.vue
@@ -1,18 +1,54 @@
 <template>
   <div>
-    <b-btn variant="primary" v-on:click="Redis.connect()">Connect</b-btn>
-    <b-btn variant="warning" v-on:click="Redis.disconnect()">Disconnect</b-btn>
-    <b-btn variant="danger" v-on:click="Redis.send(['PSUBSCRIBE','*'])">Send</b-btn>
+    <b-btn :variant="colorPrimary" v-on:click="Redis.connect()">Connect</b-btn>
+    <b-btn variant="warning" v-on:click="Redis.disconnect()">{{
+      colorPrimary
+    }}</b-btn>
+    <b-btn variant="danger" v-on:click="Redis.send(['PSUBSCRIBE', '*'])"
+      >Send</b-btn
+    >
+    <b-btn variant="danger" v-on:click="update()"
+      >Update</b-btn
+    >
   </div>
 </template>
 
 <script>
 import { Redis } from "../redis.js";
+import { Timer } from "../timer.js";
 export default {
   name: "RedisWs",
   data() {
-    return {Redis};
+    return { Redis, Timer, colorPrimary: "primary", timer:{}, count: 0 };
   },
   props: {},
+  created() {
+    this.timer = Timer.create(this.paused, 1000);
+  },
+  methods: {
+    awakeMe() {
+      this.count++;
+      if (this.count > 10) {
+        console.log("sleep");
+        Timer.delete(this.timer);
+        return;
+      }
+      console.log("awakeMe");
+      if (this.colorPrimary == "primary") {
+        this.colorPrimary = "success";
+      } else {
+        this.colorPrimary = "primary";
+      }
+    },
+    update() {
+      this.resumed()
+    },
+    paused() {
+
+    },
+    resumed(){
+      this.timer.reset();
+    }
+  },
 };
 </script>
